1b9b08b3831731b1854b9fab814865f253e8c68c
D1PT1M\DiPtim pc <oladipotimothy69@gmail.com>
2020-06-12 21:56:26 +0100

    Updates

---
+++ precache-manifest.7181ffd21d166445032abb06305b77fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a09a2b94d4782e54495b39f7fe724a7",
+    "url": "/monsterApp/index.html"
+  },
+  {
+    "revision": "c464c513547b42822373",
+    "url": "/monsterApp/static/css/main.999fad90.chunk.css"
+  },
+  {
+    "revision": "505db82e1cdadd8ef737",
+    "url": "/monsterApp/static/js/2.df5450e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterApp/static/js/2.df5450e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c464c513547b42822373",
+    "url": "/monsterApp/static/js/main.9792b0d9.chunk.js"
+  },
+  {
+    "revision": "86e8c3a6edb866607a57",
+    "url": "/monsterApp/static/js/runtime-main.590b94e3.js"
+  }
+]);
